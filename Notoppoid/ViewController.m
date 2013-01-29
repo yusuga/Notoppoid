7bc96a98384ad6756eade644d1edfd707be09c47
YuSugawara <yusuga@me.com>
2013-01-29 13:51:51 +0900

    1.0.0

--- Notoppoid/ViewController.m
+++ Notoppoid/ViewController.m
@@ -6,9 +6,17 @@
 //  Copyright (c) 2013年 Yu Sugawara. All rights reserved.
 //
 
+#import <Social/Social.h>
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *whenTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *fastSlowSegmentedControl;
+@property (weak, nonatomic) IBOutlet UITextField *areaTextField;
+@property (weak, nonatomic) IBOutlet UITextField *whereAreaTextField;
+@property (weak, nonatomic) IBOutlet UITextField *detailAreaTextField;
+@property (weak, nonatomic) IBOutlet UITextField *exactAreaTextFiled;
+@property (weak, nonatomic) IBOutlet UITextField *doingTextField;
 
 @end
 
@@ -25,5 +33,16 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)notoru:(id)sender
+{
+
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+        SLComposeViewController *vc = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [vc setInitialText:[NSString stringWithFormat:@"%@も%@から%@は%@たる%@の%@で%@中 #のとっぽいど", self.whenTextField.text, self.fastSlowSegmentedControl.selectedSegmentIndex ? @"早く" : @"遅く", self.areaTextField.text, self.whereAreaTextField.text, self.detailAreaTextField.text, self.exactAreaTextFiled.text, self.doingTextField.text]];
+        [self presentViewController:vc animated:YES completion:nil];
+    }
+}
+
 
 @end
