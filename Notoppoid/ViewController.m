dd6117c58dc29940a413895d60633ca58c1654bd
YuSugawara <yusuga@me.com>
2013-03-15 19:39:55 +0900

    ver 1.0.2

--- Notoppoid/ViewController.m
+++ Notoppoid/ViewController.m
@@ -9,6 +9,8 @@
 #import <Social/Social.h>
 #import "ViewController.h"
 
+static NSString *const kHashTag = @"#のとっぽいど";
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *whenTextField;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *fastSlowSegmentedControl;
@@ -34,26 +36,46 @@
     // Dispose of any resources that can be recreated.
 }
 
--(NSUInteger)supportedInterfaceOrientations
+- (NSUInteger)supportedInterfaceOrientations
 {
     return UIInterfaceOrientationMaskAll;
 }
 
+- (void)tweetWithNotoWords:(NSString*)notoWords
+{
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+        if (notoWords.length > 140) {
+            [[[UIAlertView alloc] initWithTitle:@"文字が長すぎますぜ" message:nil delegate:nil cancelButtonTitle:@"のとれないではないですか！！" otherButtonTitles:nil] show];
+        } else {
+            SLComposeViewController *vc = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+            [vc setInitialText:notoWords];
+            [self presentViewController:vc animated:YES completion:nil];
+        }
+    }
+}
+
+- (NSString*)notoWords
+{
+    return [NSString stringWithFormat:@"%@も%@から%@は%@たる%@の%@で%@中",
+            self.whenTextField.text.length ? self.whenTextField.text : self.whenTextField.placeholder,
+            self.fastSlowSegmentedControl.selectedSegmentIndex ? @"遅く" : @"早く",
+            self.areaTextField.text.length ? self.areaTextField.text : self.areaTextField.placeholder,
+            self.whereAreaTextField.text.length ? self.whereAreaTextField.text : self.whereAreaTextField.placeholder,
+            self.detailAreaTextField.text.length ? self.detailAreaTextField.text : self.detailAreaTextField.placeholder,
+            self.exactAreaTextFiled.text.length ? self.exactAreaTextFiled.text : self.exactAreaTextFiled.placeholder,
+            self.doingTextField.text.length ? self.doingTextField.text : self.doingTextField.placeholder];
+}
+
 - (IBAction)notoru:(id)sender
 {
+    NSString *notoWords = [NSString stringWithFormat:@"%@\n%@", [self notoWords], kHashTag];
+    [self tweetWithNotoWords:notoWords];
+}
 
-    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
-        SLComposeViewController *vc = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-        [vc setInitialText:[NSString stringWithFormat:@"%@も%@から%@は%@たる%@の%@で%@中 #のとっぽいど",
-                            self.whenTextField.text.length ? self.whenTextField.text : self.whenTextField.placeholder,
-                            self.fastSlowSegmentedControl.selectedSegmentIndex ? @"遅く" : @"早く",
-                            self.areaTextField.text.length ? self.areaTextField.text : self.areaTextField.placeholder,
-                            self.whereAreaTextField.text.length ? self.whereAreaTextField.text : self.whereAreaTextField.placeholder,
-                            self.detailAreaTextField.text.length ? self.detailAreaTextField.text : self.detailAreaTextField.placeholder,
-                            self.exactAreaTextFiled.text.length ? self.exactAreaTextFiled.text : self.exactAreaTextFiled.placeholder,
-                            self.doingTextField.text.length ? self.doingTextField.text : self.doingTextField.placeholder]];
-        [self presentViewController:vc animated:YES completion:nil];
-    }
+- (IBAction)verticalNotoru:(id)sender
+{
+    NSString *notoWords = [NSString stringWithFormat:@"%@\n%@", [self verticalStringFromNotoWords:[self notoWords]], kHashTag];
+    [self tweetWithNotoWords:notoWords];
 }
 
 - (IBAction)deleteAllText
@@ -62,6 +84,15 @@
     for (UITextField *tf in arr) {
         tf.text = nil;
     }
+}
+
+- (NSString*)verticalStringFromNotoWords:(NSString*)str
+{
+    NSMutableString *mStr = [NSMutableString string];
+    for (int i = 0; i < str.length; i++) {
+        [mStr appendFormat:@"%@\n", [str substringWithRange:NSMakeRange(i, 1)]];
+    }
+    return mStr;
 }
 
 @end
